3bfe5d13147bb272fb6e12a6ae891ba6abd07dc7
Alex-Tudor Petrean <alex@padfoot.cc>
2024-08-06 06:38:20 +0000

    change font

--- modules/wezterm/config/wezterm.lua
+++ modules/wezterm/config/wezterm.lua
@@ -1,10 +1,8 @@
 local wezterm = require("wezterm")
-local format = wezterm.format
-local nerdfonts = wezterm.nerdfonts
 
 return {
-  font = wezterm.font("FiraCode Nerd Font"),
-  font_size = 18,
+  font = wezterm.font("Cascadia Code NF"),
+  font_size = 19,
   color_scheme = "Catppuccin Mocha",
   enable_tab_bar = false,
   enable_wayland = true,

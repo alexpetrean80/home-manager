8fd51060bf48094289d85ffbc94f388a8d52e8ed
Alex-Tudor Petrean <alex-tudor.petrean@snyk.io>
2024-09-18 15:03:44 +0300

    add snyk-ls

--- modules/neovim/extraplugins.nix
+++ modules/neovim/extraplugins.nix
@@ -5,15 +5,17 @@
 }: {
   programs.nixvim.extraPlugins = with pkgs.vimPlugins; [
     lualine-nvim
+    nvim-lspconfig
     vim-tmux-navigator
     vim-test
     vimux
   ];
 
   programs.nixvim.extraConfigLua = ''
-        ${lib.readFile ./config/lualine.lua}
-        ${lib.readFile ./config/dap.lua}
+    ${lib.readFile ./config/lualine.lua}
+    ${lib.readFile ./config/dap.lua}
+    ${lib.readFile ./config/snyk-ls.lua}
 
-        vim.cmd("let test#strategy = 'vimux'")
+    vim.cmd("let test#strategy = 'vimux'")
   '';
 }

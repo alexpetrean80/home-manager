01b953a2bc0b2a9f56208f7971ae19a2b515f9f7
Alex-Tudor Petrean <alex-tudor.petrean@snyk.io>
2024-10-17 23:03:09 +0300

    change gpg key

--- hosts/mac/default.nix
+++ hosts/mac/default.nix
@@ -20,7 +20,7 @@
         contents = {
           user = {
             email = "alex-tudor.petrean@snyk.io";
-            signingkey = "E51D11C1C401B8F1FA26DA7301D2E393ABC8B299";
+            signingkey = "E99CC35A3A9AD49452F8BB98D2E6235BA0DF15D1";
           };
           commit = {
             gpgSign = true;
